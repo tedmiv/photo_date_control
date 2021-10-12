1208de3b55f0c8903ecf6a0892a486916093d3e3
Dmytro Tereshchenko <tedmiv@gmail.com>
2021-10-12 21:29:59 +0300

    File open dialog

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello, world!")
-print("Here the change!")
+import os
+from tkinter import filedialog
+import tkinter as tk
+
+print(os.getcwd())
+root = tk.Tk()
+root.withdraw()
+root.filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select file",
+                                           filetypes=[("Image Files", "*.jpg")])
+print(root.filename)
