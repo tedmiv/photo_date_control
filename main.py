23f25160b6d66f3369d79f93f0cc27a9eb5e11f3
Dmytro Tereshchenko <tedmiv@gmail.com>
2022-03-07 15:59:16 +0200

    Open directory

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+"""
+The program to automatic synchronize EXIF date data and filename
+"""
+
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askdirectory
+from os import listdir
+from os.path import isfile, join
+from exif import Image
 
 window = tk.Tk()
 window.withdraw()
-filename = askopenfilename()
+directoryName = askdirectory(title="Choose the directory with photo files for \
+    change", initialdir="/")
+print(directoryName)
+dirItems = listdir(directoryName)
+files = list()
+for file in dirItems:
+    if isfile(join(directoryName, file)):
+        fileExtension = file.split('.')
+        if fileExtension[-1].lower() == "jpg":
+            files.append(file)
+print(files)
+
+print('Ok')
+
+window.mainloop()
