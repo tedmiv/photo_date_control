b7ded99dadfbd663a9134fc0485508444f5571e5
Dmytro Tereshchenko <tedmiv@gmail.com>
2022-03-07 12:43:04 +0200

    Rename project

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-from tkinter import filedialog
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
 
-print(os.getcwd())
-root = tk.Tk()
-root.withdraw()
-root.filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select file",
-                                           filetypes=[("Image Files", "*.jpg")])
-print(root.filename)
+window = tk.Tk()
+window.withdraw()
+filename = askopenfilename()
